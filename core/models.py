5e2099616fdfc0f85b5809b5ae2a57e7d534c6ba
“krv006” <rvkamronbek@gmail.com>
2024-06-10 21:33:55 +0500

    add

--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=355)
+    description = models.TextField()
+    image = models.ImageField(upload_to='/news')
+
+    def __str__(self):
+        return self.title
+
+
+class Media(models.Model):
+    icon = models.CharField()
+    url = models.URLField()
+
+
+class Partner(models.Model):
+    image = models.ImageField(upload_to='/partners')
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
+
+
+class Worker(models.Model):
+    full_name = models.CharField(max_length=50)
+    work = models.CharField(max_length=55)
+    image = models.ImageField(upload_to='/workers')
+
+    def __str__(self):
+        return self.full_name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=355)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/projects')
+
+    def __str__(self):
+        return self.title
